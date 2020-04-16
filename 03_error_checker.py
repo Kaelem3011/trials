740779e63999185ff2db6deb8224fcfe636e0194
Kaelem <51727185+Kaelem3011@users.noreply.github.com>
2020-04-17 09:45:56 +1200

    03_error_checker

---
+++ 03_error_checker.py
@@ -0,0 +1,33 @@
+# Not Blank Function goes here
+def not_blank(question, error_msg, num_ok):
+    error = error_msg
+
+    valid = False
+    while not valid:
+        response = input(question)
+        has_errors = ""
+
+        if num_ok != "yes":
+            # look at each character in string and if it's a number, complain
+            for letter in response:
+                if letter.isdigit():
+                    has_errors = "yes"
+                    break
+
+        if response == "":
+            print(error)
+            continue
+        elif has_errors != "":
+            print(error)
+            continue
+        else:
+            return response
+
+
+# main routine goes here
+
+test = not_blank("enter done to pass the test ",
+                 "Your answer can not be blank or contain numbers",
+                 "no")
+
+print("you are {} with the test".format(test))
