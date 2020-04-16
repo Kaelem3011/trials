7bb16a8d60f2d8e17be314949599ce6c8dc88415
Kaelem <51727185+Kaelem3011@users.noreply.github.com>
2020-04-17 09:45:37 +1200

    01_instructions

---
+++ 01_instructions.py
@@ -0,0 +1,8 @@
+# prints instructions
+print("Welcome to the ball review generator")
+print("The review generator rates the sports that your preferred ball is used to play")
+print("This generator talks about what your preferred ball say's about you. "
+      "Remember to take all the reviews with a grain of salt except for golf because it will never be a sport")
+print("Make sure that your answers are all spelt correctly or an error message will be displayed")
+
+
