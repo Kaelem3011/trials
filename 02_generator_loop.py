5fd143c823a279df6183ee38b1e9cb773a1304a1
Kaelem <51727185+Kaelem3011@users.noreply.github.com>
2020-04-17 09:45:47 +1200

    02_generator_loop

---
+++ 02_generator_loop.py
@@ -0,0 +1,6 @@
+keep_going = ""
+while keep_going == "":
+
+    print("This generator reviews what a ball say's about you")
+
+    keep_going = input("To enter another ball press enter or press any other key to quit")
